98da08b6cf8061908e253d0feb7c02039adff08b
Али Дотмирзаев <ali@MacBook-Pro-Ali.fritz.box>
2026-04-09 15:21:47 +0200

    add_ts_tasks

---
+++ src/app/training.ts
@@ -0,0 +1,67 @@
+let uploadStatus: "loading" | "success" | "error";
+
+let textFormat: "uppercase" | "lowercase" | "capitalize";
+
+interface IUser {
+    name: string;
+    surname: string;
+    email: string;
+    age?: number;
+    isMarried: boolean;
+}
+
+interface IPosition extends IUser {
+    role: string;
+    salary: number;
+}
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function formatText(text: string, format: "uppercase" | "lowercase" | "capitalize"): string {
+    if (format === "uppercase") {
+        return text.toUpperCase();
+    } else if (format === "lowercase") {
+        return text.toLowerCase();
+    } else if (format === "capitalize") {
+        return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    } else {
+        return text;
+    }
+}
+
+function removeSymbol(text: string, symbol: string): string {
+    return text.replaceAll(symbol, "");
+}
+
+const users: IUser[] = [
+    {
+        name: "Ali",
+        surname: "Dotmirzaev",
+        email: "ddd@mail.com",
+        age: 33,
+        isMarried: true
+    },
+    {
+        name: "Vlad",
+        surname: "Babenko",
+        email: "vvv@gmail.com",
+        age: 21,
+        isMarried: false
+    },
+    {
+        name: "Farukh",
+        surname: "Safarov",
+        email: "sss@gmail.com",
+        isMarried: true
+    }
+]
+
+const happyUser: IUser[] = users.filter(user => !user.isMarried);
+
+happyUser.forEach(user => {
+    console.log(`Самый довольный тут: ${user.name}`);
+})
+
+
